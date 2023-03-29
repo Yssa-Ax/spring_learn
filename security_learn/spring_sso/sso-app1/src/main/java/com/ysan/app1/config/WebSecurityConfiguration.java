54c4a7695d7d0a197111f7f8a1e8c286f0d99042
ysan490930 <798687064@qq.com>
2023-03-29 15:25:16 +0800

    Security_Learn

---
+++ security_learn/spring_sso/sso-app1/src/main/java/com/ysan/app1/config/WebSecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.ysan.app1.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableOAuth2Sso
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.logout()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+}
---
+++ security_learn/spring_sso/sso-app2/src/test/java/com/ysan/app2/SsoApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysan.app2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
